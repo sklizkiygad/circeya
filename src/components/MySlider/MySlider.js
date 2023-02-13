4d0352c1cbd79e9bebcddc485cb2e1da5a986370
work-sklizkiygad <73179568+sklizkiygad@users.noreply.github.com>
2023-02-13 19:36:46 +0300

    slider

---
+++ src/components/MySlider/MySlider.js
@@ -0,0 +1,151 @@
+import React, {useEffect, useRef, useState} from "react";
+// Import Swiper React components
+import {Swiper,SwiperSlide, useSwiper} from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "./MySlider.css";
+import {Swiper as bibaSwiper,Mousewheel} from "swiper";
+
+import slide1 from '../../assets/images/slide1.png';
+import slide2 from '../../assets/images/slide2.png';
+import slide3 from '../../assets/images/slide3.png';
+import slide4 from '../../assets/images/slide4.png';
+import slide5 from '../../assets/images/slide5.png';
+import slide6 from '../../assets/images/slide6.png';
+import slide7 from '../../assets/images/slide7.png';
+import slide8 from '../../assets/images/slide8.png';
+
+import SwiperCore from 'swiper';
+import {useScrollBlock} from "../util/UseScrollBlock";
+
+SwiperCore.use([Mousewheel]);
+
+
+
+export default function MySlider() {
+
+
+
+
+    const [isScrolling,setIsScrolling]=useState(false)
+    const [isBodyScrolling, setIsBodyScrolling]=useState(true)
+    const [documentTop,setDocumentTop]=useState(0)
+    const scrollArea=useRef();
+
+    const [blockScroll, allowScroll] = useScrollBlock();
+
+
+
+
+    useEffect(()=>{
+
+        if(isBodyScrolling){
+
+            document.getElementsByTagName('body')[0].classList.remove('no-scroll')
+
+        }
+        else {
+            document.getElementsByTagName('body')[0].classList.add('no-scroll')
+
+        }
+
+    },[isBodyScrolling])
+
+
+    const options={
+        root:null,
+        rootMargin:"0px",
+        threshold:1
+    }
+    const callbackFunction=(entries)=>{
+
+
+
+
+        if(entries[0].isIntersecting){
+            setIsScrolling(true)
+            console.log('есть скролл')
+            blockScroll()
+
+        }
+
+
+
+    }
+
+    useEffect(()=>{
+        const observer = new IntersectionObserver(callbackFunction, options)
+        observer.observe(scrollArea.current)
+    },[])
+
+    const lastSlide=()=>{
+        setIsScrolling(false)
+        allowScroll()
+    }
+
+
+
+
+
+
+    return (
+        <>
+            <h2 className='main-block__heading'>Lorem ipsum dolor sit amet</h2>
+            <Swiper
+                slidesPerView={5}
+                modules={Mousewheel}
+                direction={"horizontal"}
+                mousewheel={isScrolling}
+                centeredSlides={true}
+                onReachEnd={() => lastSlide()}
+                onReachBeginning={() => lastSlide()}
+                className="mySwiper"
+                ref={scrollArea}
+            >
+                <SwiperSlide><img className='main-slide' src={slide1}/></SwiperSlide>
+                <SwiperSlide><img className='main-slide' src={slide2}/></SwiperSlide>
+                <SwiperSlide><img className='main-slide' src={slide3}/></SwiperSlide>
+                <SwiperSlide><img className='main-slide' src={slide4}/></SwiperSlide>
+                <SwiperSlide><img className='main-slide' src={slide5}/></SwiperSlide>
+                <SwiperSlide><img className='main-slide' src={slide6}/></SwiperSlide>
+                <SwiperSlide><img className='main-slide' src={slide7}/></SwiperSlide>
+                <SwiperSlide><img className='main-slide' src={slide8}/></SwiperSlide>
+
+                <SwiperButtonNext isScrolling={isScrolling}/>
+
+            </Swiper>
+
+
+
+
+        </>
+    );
+}
+const SwiperButtonNext = ({isScrolling}) => {
+
+    const swiper = useSwiper();
+    let [isSwiperFound,setIsSwiperFound]=useState(false);
+    useEffect(()=>{
+
+        if(swiper.mousewheel){
+            setIsSwiperFound(true)
+        }
+
+    },[])
+
+    useEffect(()=>{
+
+if(isScrolling && isSwiperFound){
+
+    swiper.mousewheel.enable()
+}
+else if(isSwiperFound && !isScrolling){
+    swiper.mousewheel.disable()
+}
+
+    },[isScrolling,isSwiperFound])
+
+
+    return <></>
+};
--- src/pages/MainPage/MainPage.js
+++ src/pages/MainPage/MainPage.js
@@ -3,12 +3,14 @@ import './MainPage.css';
 import MyHeader from "../../components/MyHeader/MyHeader";
 import MainBlock from "../../components/MainBlock/MainBlock";
 import MyFooter from "../../components/MyFooter/MyFooter";
+import MySlider from "../../components/MySlider/MySlider";
 
 const MainPage = () => {
     return (
         <div className="main-page">
             <MyHeader/>
             <MainBlock firstImage={'img1'} secondImage={'img2'}/>
+            <MySlider/>
             <MainBlock firstImage={'img3'} secondImage={'img4'}/>
             <MyFooter/>
         </div>
